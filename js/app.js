8305f381155d631d8b01b0b40a54d3708bf53817
aliakbarpirnazar <aliakbarpirnazar@gmail.com>
2023-11-25 12:55:01 +0330

    Update 2

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
 const taskInput = document.getElementById("task-input");
 const dateInput = document.getElementById("date-input");
 const addButton = document.getElementById("add-button");
+const editButton = document.getElementById("edit-button");
 const alertMessage = document.getElementById("alert-message");
+const todosBody = document.querySelector("tbody");
+const deleteAllButton = document.getElementById("delete-all-button");
 
-const todos = [];
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+const generateID = () => {
+  return Math.round(
+    Math.random() * Math.random() * Math.pow(10, 15)
+  ).toString();
+};
+
+const saveToLocalStorage = () => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
 
 const showAlert = (message, type) => {
   alertMessage.innerHTML = "";
@@ -18,23 +31,87 @@ const showAlert = (message, type) => {
   }, 2000);
 };
 
+const displayTodos = () => {
+  todosBody.innerHTML = "";
+  if (!todos.length) {
+    todosBody.innerHTML = "<tr><td colspan='4'>No task found!</td></tr>";
+    return;
+  } else {
+    todos.forEach((todo) => {
+      todosBody.innerHTML += `
+      <tr>
+      <td>${todo.task}</td>
+      <td>${todo.date || "No date"}</td>
+      <td>${todo.completed ? "Completed" : "Pending"}</td>
+      <td>
+          <button onclick="editHandler('${todo.id}')">Edit</button>
+          <button onclick="toggleHandler('${todo.id}')">${
+        todo.completed ? "Undo" : "Do"
+      }</button>
+          <button onclick="deleteHandler('${todo.id}')">Delete</button>
+      </td>
+      </tr>
+      `;
+    });
+  }
+};
+
 const addHandler = () => {
   const task = taskInput.value;
   const date = dateInput.value;
   const todo = {
-    task: task,
-    date: date,
+    id: generateID(),
+    task,
+    date,
     completed: false,
   };
   if (task && date) {
     todos.push(todo);
+    saveToLocalStorage();
+    displayTodos();
     taskInput.value = "";
     dateInput.value = "";
-    console.log(todos);
     showAlert("Todo added successfully", "success");
   } else {
     showAlert("Please enter a todo!", "error");
   }
 };
 
+const deleteAllHandler = () => {
+  if (todos.length) {
+    todos = [];
+    saveToLocalStorage();
+    displayTodos();
+    showAlert("All todos cleared successfuly", "success");
+  } else {
+    showAlert("No todos to clear", "error");
+  }
+};
+
+const deleteHandler = (id) => {
+  const newTodos = todos.filter((todo) => todo.id !== id);
+  todos = newTodos;
+  saveToLocalStorage();
+  displayTodos();
+  showAlert("Todo deleted successfuly", "success");
+};
+
+const toggleHandler = (id) => {
+  const todo = todos.find((todo) => todo.id === id);
+  todo.completed = !todo.completed;
+  saveToLocalStorage();
+  displayTodos();
+  showAlert("Todo status change successfuly", "success");
+};
+
+const editHandler = (id) => {
+  const todo = todos.find((todo) => todo.id === id);
+  taskInput.value = todo.task;
+  dateInput.value = todo.date;
+  addButton.style.display = "none";
+  editButton.style.display = "inline-block";
+};
+
+window.addEventListener("load", displayTodos);
 addButton.addEventListener("click", addHandler);
+deleteAllButton.addEventListener("click", deleteAllHandler);
